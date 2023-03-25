7689f57a39a7d441603cfd6703825ad71c34f380
Andrii Kushynskyi <akushyn@gmail.com>
2023-03-25 18:45:28 +0700

    implement simple discord bot app

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+import discord
+
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="-", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('My Name is akushyn!!!')
+
+
+bot.run(DISCORD_TOKEN)
+print('success')
