fc87aca5f8da3f8998118cdfdd3bcd373d4e8272
Andrii Kushynskyi <akushyn@gmail.com>
2023-04-01 00:56:13 +0700

    add MATCH_TOKEN_RATIO config

--- config.py
+++ config.py
@@ -9,3 +9,6 @@ PRIVILEGED_USER_ROLES = os.getenv('PRIVILEGED_USER_ROLES')
 
 MEMBER_UPDATE_KICK_REASON = "Nickname changed to a privileged user"
 MEMBER_JOIN_KICK_REASON = "Nickname set to a privileged user on join"
+
+# how many % changed nickname should match with roles
+MATCH_TOKEN_RATIO = 100
