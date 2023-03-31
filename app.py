91e1cf7ec1277d489eb219168b7c7a077f80cdc6
Andrii Kushynskyi <akushyn@gmail.com>
2023-03-31 21:40:20 +0700

    check on member update, join

--- app.py
+++ app.py
@@ -10,6 +10,29 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="-", intents=intents)
 
 
+client = discord.Client()
+
+
+async def check_nickname_change(member):
+    privileged_roles = ["Admin", "Moderator"] # Change this to the roles you want to check for
+    for role in member.roles:
+        if role.name in privileged_roles and member.nick is not None and role.name not in member.nick:
+            await member.kick(reason="Changed nickname to privileged user")
+            break
+
+
+@client.event
+async def on_member_update(before, after):
+    if before.nick != after.nick:
+        await check_nickname_change(after)
+
+
+@client.event
+async def on_member_join(member):
+    await check_nickname_change(member)
+
+
+client.run("TOKEN")
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
