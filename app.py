68cfbe1f0766010ae70fa1de2bfd9a5ef2f5e3c6
Andrii Kushynskyi <akushyn@gmail.com>
2023-03-31 23:49:30 +0700

    add config.py

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
 import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
+import config
 
 
-load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-PRIVILEGED_USERS = os.getenv('PRIVILEGED_USERS')
-PRIVILEGED_USERS = PRIVILEGED_USERS.split(',')
+def get_privileged_roles():
+    """
+    Get parsed privileged roles
+    :return: List of privileged roles
+    """
+    return [u.strip() for u in config.PRIVILEGED_USER_ROLES.split(',')]
 
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="-", intents=intents)
 
 
+def is_privileged_nick(nick):
+    """
+    Check whether member nick equal to one of privileged roles
+    :param nick: Member nickname
+    :return: Returns True if the nickname equal to privileged roles, False otherwise.
+    """
+    if nick is not None and nick.lower() in get_privileged_roles():
+        return True
+    return False
+
+
 def check_nickname_change(before, after):
     """
     Checks if a member's nickname has been changed to a privileged user.
-    Returns True if the nickname has been changed, False otherwise.
+    :param before: Member before
+    :param after: Member after
+    :return: Returns True if the nickname has been changed, False otherwise.
     """
     if before.nick != after.nick:
-        if after.nick is not None and after.nick.lower() in PRIVILEGED_USERS:
+        if is_privileged_nick(after.nick):
             return True
     return False
 
@@ -33,13 +48,13 @@ async def on_ready():
 @bot.event
 async def on_member_update(before, after):
     if check_nickname_change(before, after):
-        await after.kick(reason="Nickname changed to a privileged user")
+        await after.kick(reason=config.MEMBER_UPDATE_KICK_REASON)
 
 
 @bot.event
 async def on_member_join(member):
-    if member.nick is not None and member.nick.lower() in PRIVILEGED_USERS:
-        await member.kick(reason="Nickname set to a privileged user on join")
+    if member.nick is not None and member.nick.lower() in get_privileged_roles():
+        await member.kick(reason=config.MEMBER_JOIN_KICK_REASON)
 
 
-bot.run(DISCORD_TOKEN)
+bot.run(config.DISCORD_TOKEN)
---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+PRIVILEGED_USER_ROLES = os.getenv('PRIVILEGED_USER_ROLES')
+
+MEMBER_UPDATE_KICK_REASON = "Nickname changed to a privileged user"
+MEMBER_JOIN_KICK_REASON = "Nickname set to a privileged user on join"
