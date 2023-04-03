d59d851af8e1dc47d8cc50dba79e9cde7641037b
Andrii Kushynskyi <akushyn@gmail.com>
2023-04-03 14:33:32 +0700

    update configurations, env.example

--- config.py
+++ config.py
@@ -7,8 +7,12 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 PRIVILEGED_USER_ROLES = os.getenv('PRIVILEGED_USER_ROLES')
 
-MEMBER_UPDATE_KICK_REASON = "Nickname changed to a privileged user"
-MEMBER_JOIN_KICK_REASON = "Nickname set to a privileged user on join"
+MEMBER_NICK_DUPLICATE_REASON = "Member 'nickname' changed to a privileged user"
+MEMBER_NAME_DUPLICATE_REASON = "Name changed to a privileged user"
+MEMBER_DISPLAY_NAME_DUPLICATE_REASON = "Display Name changed to a privileged user"
 
 # how many % changed nickname should match with roles
 MATCH_TOKEN_RATIO = 100
+
+# discord server ID
+DISCORD_SERVER_ID = os.getenv('DISCORD_SERVER_ID')
