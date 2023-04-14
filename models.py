f0f3160bcab7719d021e69805c43682e7c23710c
Andrii Kushynskyi <akushyn@gmail.com>
2023-04-14 20:47:41 +0700

    add alembic configurations

---
+++ models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+# Connect to the database
+from sqlalchemy.orm import sessionmaker
+
+import config
+
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+Base = declarative_base()
+
+
+# Define the members table
+class Member(Base):
+    __tablename__ = 'members'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    display_name = Column(String)
+    nick = Column(String)
+    updated_at = Column(DateTime)
+
+    def __init__(self, id, name, last_seen):
+        self.id = id
+        self.name = name
+        self.last_seen = last_seen
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
