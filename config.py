97ca50f3797533c2327cef7a632b8a75b48e8dda
Andrii Kushynskyi <akushyn@gmail.com>
2023-04-14 22:43:01 +0700

    fix check period in seconds

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ MEMBER_NICK_DUPLICATE_REASON = "Member used 'nickname' of a privileged user"
 MEMBER_NAME_DUPLICATE_REASON = "Member used 'name' of a privileged user"
 MEMBER_DISPLAY_NAME_DUPLICATE_REASON = "Member used 'display_name' of a privileged user"
 
+# duration in days to check members
+MEMBER_CHECK_PERIOD = 60*60*24
 # how many % changed nickname should match with roles
 MATCH_TOKEN_RATIO = 100
 
