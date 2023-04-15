badf7fed1ba9ab5b507856a19a90044d56ff92c8
Andrii Kushynskyi <akushyn@gmail.com>
2023-04-15 15:44:41 +0700

    remove db, cleanup requirements.

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ MEMBER_NICK_DUPLICATE_REASON = "Member used 'nickname' of a privileged user"
 MEMBER_NAME_DUPLICATE_REASON = "Member used 'name' of a privileged user"
 MEMBER_DISPLAY_NAME_DUPLICATE_REASON = "Member used 'display_name' of a privileged user"
 
-# duration in days to check members
-MEMBER_CHECK_PERIOD = 60*60*24
+# duration in seconds to check members
+# MEMBER_CHECK_PERIOD = 60*60*24
+MEMBER_CHECK_PERIOD = 60*1*1
 # how many % changed nickname should match with roles
 MATCH_TOKEN_RATIO = 100
 
 # discord server ID
 DISCORD_SERVER_ID = os.getenv('DISCORD_SERVER_ID')
-SQLALCHEMY_DATABASE_URI = "sqlite:///bot.db"
